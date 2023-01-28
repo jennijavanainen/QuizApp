ad294e9171292155ab8e65d2daa6d11aadd7e210
jennijavanainen <jenni.kupukupu@gmail.com>
2023-01-28 19:22:43 +0200

    Answer check, score count. Still buggy.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,23 @@
 import Form from 'react-bootstrap/Form'
 import { useState } from 'react'
 
-const Question = ({ q }) => {
+const Question = ({ q, answer }) => {
   const [selected, setSelected] = useState(null)
   const handleOptionChange = (option) => {
     setSelected(option.target.value)
+    answer(option.target.value)
   }
   console.log("selected: ", selected)
 
   return (
     q && <div>
       <h4>{q.question}</h4>
-      <Form>
+      <Form.Group>
         {q.answers.map(a =>
           <div key={a.id} className='mb-3'>
             <Form.Check type='radio' id={a.id} label={a.option} value={a.option} checked={selected === a.option} onChange={handleOptionChange}/>
           </div> )}
-      </Form>
+      </Form.Group>
     </div>
   )
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,33 @@
 import Question from './Question'
+import { Form, Button } from 'react-bootstrap'
+import { useState } from 'react'
 
 const Quiz = ({ quiz }) => {
+
+  const [score, setScore] = useState(0)
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log('score: ', score)
+  }
+
+  const checkAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      setScore(score + 1)
+    }
+  }
+
 
   if (quiz) {
     return (
       <div>
         <h2>{quiz.name}</h2>
-        {quiz.questions.map(q => <Question key={q.id} q={q} />)}
+        <Form onSubmit={onSubmit}>
+          {quiz.questions.map(q => <Question key={q.id} q={q} answer={a => checkAnswer(a, q.correct.answer)} />)}
+          <Button variant='primary' type='submit'>
+            save
+          </Button>
+        </Form>
       </div>
     )
   }
